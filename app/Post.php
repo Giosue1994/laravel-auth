96b0a86dba7b212a0e3f5546ea70fbb973c15147
Giosue1994 <giosuelentini1994@outlook.com>
2020-09-14 16:23:45 +0200

    creazione tabelle e model

---
+++ app/Post.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = [
+      'user_id',
+      'title',
+      'image',
+      'content',
+    ];
+
+    public function users() {
+      return $this->belongsTo('App\User');
+    }
+}
